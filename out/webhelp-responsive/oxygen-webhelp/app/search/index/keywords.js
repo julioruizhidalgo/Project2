e9145432ecec96052eec586fefb61f6939039b94
julioruizhidalgo <57219609+julioruizhidalgo@users.noreply.github.com>
2019-11-24 21:16:18 +0000

    another try

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,32 @@
+define(function() {var keywords=[{w:"About",p:["p0"]},{w:"this",p:["p0"]},{w:"Publication",p:["p0"]},{w:"Applications",p:["p1"]},{w:"of",p:["p1"]},{w:"IBM",p:["p1","p2","p4","p5","p6","p7","p8","p9","p10","p14","p17","p18","p19"]},{w:"StoredIQ",p:["p1","p2","p4","p5","p6","p7","p8","p9","p10","p14","p17","p18","p19"]},{w:"Default",p:["p3"]},{w:"user",p:["p3"]},{w:"accounts",p:["p3"]},{w:"Administrator",p:["p4"]},{w:"components",p:["p5","p16"]},{w:"Data",p:["p6","p7","p8","p22"]},{w:"Server",p:["p6","p22"]},{w:"Workbench",p:["p7"]},{w:"Desktop",p:["p8","p22"]},{w:"Collector",p:["p8"]},{w:"Insights",p:["p9"]},{w:"Policy",p:["p10"]},{w:"Manager",p:["p10"]},{w:"License",p:["p11"]},{w:"usage",p:["p11"]},{w:"metrics",p:["p11"]},{w:"Network",p:["p12"]},{w:"and",p:["p12"]},{w:"port",p:["p12"]},{w:"requirements",p:["p12","p13"]},{w:"Open",p:["p13","p22"]},{w:"Virtual",p:["p13","p20"]},{w:"Appliance",p:["p13"]},{w:"(OVA)",p:["p13"]},{w:"configuration",p:["p13"]},{w:"product",p:["p14"]},{w:"library",p:["p14"]},{w:"Security",p:["p15"]},{w:"Solution",p:["p16"]},{w:"Contacting",p:["p17"]},{w:"customer",p:["p17"]},{w:"support",p:["p17"]},{w:"Deploying",p:["p18","p19","p20"]},{w:"on",p:["p18"]},{w:"Microsoft",p:["p18"]},{w:"Hyper-V",p:["p18"]},{w:"the",p:["p20","p22"]},{w:"appliances",p:["p20"]},{w:"Environment",p:["p21"]},{w:"sizing",p:["p21"]},{w:"guidelines",p:["p21"]},{w:"ports",p:["p22"]},{w:"for",p:["p22","p23"]},{w:"client",p:["p22"]},{w:"access",p:["p22"]},{w:"to",p:["p22"]},{w:"Planning",p:["p23"]},{w:"deployment",p:["p23"]},{w:"Stack-provisioning",p:["p24"]},{w:"prerequisites",p:["p24"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6];
+ph["p2"]=[5, 6];
+ph["p3"]=[7, 8, 9];
+ph["p4"]=[5, 6, 10];
+ph["p5"]=[5, 6, 11];
+ph["p6"]=[5, 6, 12, 13];
+ph["p7"]=[5, 6, 12, 14];
+ph["p8"]=[5, 6, 15, 12, 16];
+ph["p9"]=[5, 6, 17];
+ph["p10"]=[5, 6, 18, 19];
+ph["p12"]=[23, 24, 25, 26];
+ph["p11"]=[20, 21, 22];
+ph["p14"]=[5, 6, 32, 33];
+ph["p13"]=[27, 28, 29, 30, 31, 26];
+ph["p16"]=[35, 11];
+ph["p15"]=[34];
+ph["p18"]=[39, 5, 6, 40, 41, 42];
+ph["p17"]=[36, 5, 6, 37, 38];
+ph["p19"]=[39, 5, 6];
+ph["p21"]=[45, 46, 47];
+ph["p20"]=[39, 43, 28, 44];
+ph["p23"]=[53, 49, 54];
+ph["p22"]=[27, 48, 49, 15, 50, 51, 52, 43, 12, 13];
+ph["p24"]=[55, 56];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {20:0,8:1,6:1,25:12,23:22,9:1,27:26,14:0,18:5,21:22,7:1,10:1,5:-1,1:5,22:-1,4:1,13:26,15:26,24:26,26:-1,2:-1,11:26,12:26,3:15,0:-1};
+});
